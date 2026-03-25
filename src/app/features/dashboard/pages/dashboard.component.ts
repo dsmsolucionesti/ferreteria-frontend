d31ba2234079846e19e90e15f66e569e62689134
Diego Patricio Muñoz Mauro <dmunoz@aligare.com>
2026-03-25 11:58:38 -0300

    Menu colapsable de primeng pendiente de color estable

--- src/app/features/dashboard/pages/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard.component.ts
@@ -1,40 +1,72 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { PanelMenuModule } from 'primeng/panelmenu';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, PanelMenuModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-  menu = [
+  menu: MenuItem[] = [
     {
       label: 'Inicio',
-      route: '',
-      dropdown: false,
+      icon: 'pi pi-home',
+      routerLink: '/',
     },
     {
       label: 'Cotizaciones',
-      dropdown: true,
-      child: [
-        { label: 'Cotizaciones', route: '/cotizaciones' },
-        { label: 'Productos', route: '/productos' },
-        { label: 'Categorías', route: '/categorias' },
+      icon: 'pi pi-file',
+      items: [
+        {
+          label: 'Cotizaciones',
+          icon: 'pi pi-file',
+          routerLink: '/cotizaciones',
+        },
+        {
+          label: 'Productos',
+          icon: 'pi pi-barcode',
+          routerLink: '/productos',
+        },
+        {
+          label: 'Categorías',
+          icon: 'pi pi-list',
+          routerLink: '/categorias',
+        },
       ],
     },
-    { label: 'Nota de venta', route: '/nota-venta', dropdown: false },
-    { label: 'Pagos', route: '/pagos', dropdown: false },
+    {
+      label: 'Nota de venta',
+      icon: 'pi pi-shopping-cart',
+      routerLink: '/nota-venta',
+    },
+    {
+      label: 'Pagos',
+      icon: 'pi pi-credit-card',
+      routerLink: '/pagos',
+    },
     {
       label: 'Mantenedores',
-      route: '/mantenedores',
-      dropdown: true,
-      child: [
-        { label: 'Clientes', route: '/mantenedores/clientes' },
-        { label: 'Usuarios', route: '/mantenedores/usuarios' },
-        { label: 'Ver todos', route: '/mantenedores' },
+      icon: 'pi pi-cog',
+      items: [
+        {
+          label: 'Clientes',
+          icon: 'pi pi-users',
+          routerLink: '/mantenedores/clientes',
+        },
+        {
+          label: 'Usuarios',
+          icon: 'pi pi-user',
+          routerLink: '/mantenedores/usuarios',
+        },
+        {
+          label: 'Ver todos',
+          icon: 'pi pi-angle-double-down',
+          routerLink: '/mantenedores',
+        },
       ],
     },
   ];
