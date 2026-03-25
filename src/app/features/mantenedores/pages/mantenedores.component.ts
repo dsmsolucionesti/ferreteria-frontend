d8e38c80aeb61392170961f91e7ee75415be4898
Diego Patricio Muñoz Mauro <dmunoz@aligare.com>
2026-03-25 10:12:57 -0300

    homologación de estilos por cajas

--- src/app/features/mantenedores/pages/mantenedores.component.ts
+++ src/app/features/mantenedores/pages/mantenedores.component.ts
@@ -10,44 +10,16 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['./mantenedores.component.css'],
 })
 export class MantenedoresComponent {
-  title = 'Mantenedores';
-
   items = [
     {
       label: 'Clientes',
       description: 'Gestión de clientes',
-      icon: 'pi pi-users',
       route: '/mantenedores/clientes',
     },
     {
       label: 'Usuarios',
       description: 'Gestión de usuarios',
-      icon: 'pi pi-user',
       route: '/mantenedores/usuarios',
     },
-    {
-      label: 'Roles',
-      description: 'Gestión de roles',
-      icon: 'pi pi-shield',
-      route: '/mantenedores/roles',
-    },
-    {
-      label: 'Permisos',
-      description: 'Gestión de permisos',
-      icon: 'pi pi-key',
-      route: '/mantenedores/permisos',
-    },
-    {
-      label: 'Tipos de documento',
-      description: 'Gestión de tipos de documento',
-      icon: 'pi pi-file',
-      route: '/mantenedores/tipos-documento',
-    },
-    {
-      label: 'Tipos de cliente',
-      description: 'Gestión de tipos de cliente',
-      icon: 'pi pi-user',
-      route: '/mantenedores/tipos-cliente',
-    }
   ];
 }
